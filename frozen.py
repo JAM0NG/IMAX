98ecfdd1dc7b2247370daf4ff1390ba03a7a76d6
JAM0NG <57297801+JAM0NG@users.noreply.github.com>
2019-11-12 21:58:36 +0900

    Update frozen.py

--- frozen.py
+++ frozen.py
@@ -1,6 +1,7 @@
 import requests
 import telepot
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 token = "1042804802:AAG4SbkWhnOyvor8l6SDFgZ8bae1fqljfq4"
 bot = telepot.Bot(token)
@@ -12,6 +13,7 @@ html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 list = soup.select('.info-hall')
 
-
 if '겨울' in html and 'IMAX' in str(list):
     bot.sendMessage(chat_id='906326133', text="겨울왕국 2 IMAX 예매 열림")
+else:
+    bot.sendMessage(chat_id='906326133', text="알리미 정상 작동 중 !!")
